7df7600f7b45004190f5e76a4c8c25dd44e86d58
Aureluk <122380412+joaoaureluk@users.noreply.github.com>
2023-03-06 15:19:03 -0300

    Update calculo.js

--- calculo.js
+++ calculo.js
@@ -1,7 +1,20 @@
 
+
+
 const botao = document.querySelector("#botao");
 
 botao.addEventListener("click", add);
+
+document.addEventListener("keypress", function(e){
+
+  if(e.key === "Enter"){
+
+ botao.click()    
+  }
+} )
+
+
+
 
 
 function add() {
@@ -11,6 +24,37 @@ function add() {
   const valor = document.querySelector("#valor");
   const ano = valor.value; 
 
+
+
+if (isNaN(ano) || ano < 1) {
+  
+  var text = "Hahahahaha acho que eu nao iria pensar nisso ksskkssk boa tentativa, tenta outra";
+  document.getElementById("aviso").innerHTML = text;
+  return
+} 
+else if (ano <=1800){
+ text = "se nao nasceu nessa epoca amigão"
+ document.getElementById("aviso").innerHTML = text;
+ return
+}
+else if (ano <=1900 ){
+  text = "meio veio mas tabom né"
+  document.getElementById("aviso").innerHTML = text;
+} 
+else if (ano > 1900){
+  text = "tudo certo meu nobre";
+  document.getElementById("aviso").innerHTML = text;
+  
+} 
+
+  
+  
+
+
+
+ 
+  
+
   const dataAtual = new Date();
   const anoAtual = dataAtual.getFullYear();
 
@@ -18,7 +62,7 @@ function add() {
   const resultado =anoAtual - ano;
 
 
-  document.querySelector("#result").innerHTML = "Você "+name + " tem "+ resultado+" Anos ou vai ter";
+  document.querySelector("#result").innerHTML = `Você ${name}  tem  ${resultado} Anos ou vai ter`;
 
   console.log("O", resultado);
   
